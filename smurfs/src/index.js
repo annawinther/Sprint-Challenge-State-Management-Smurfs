fe67c3907c8a4ce476cfe74a312675b0a5d104da
Anna W <anski.anna@gmail.com>
2019-10-11 18:47:58 +0100

    added files

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from 'redux-thunk';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+    rootReducer,
+    {},
+    compose(
+        applyMiddleware(thunk),   
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    )
+)
+
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
